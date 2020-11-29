466ab4e6786452067335875ea2f06c44aa79a1d7
Alejandro Napoles <alejandro@alejandronapoles.com>
2020-11-29 18:30:43 +0000

    Set up project scaffolding.
    - Linting.
    - Test framework.
    - Typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+export default {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    node: true,
+    jest: true,
+    es2020: true,
+  },
+};
