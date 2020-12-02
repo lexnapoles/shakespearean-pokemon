b2a14e433241efd7676c19e95578aaff0f6009e1
Alejandro Gonzalez <agonzalez@findmypast.com>
2020-12-02 20:44:55 +0000

    Implemented first adapter for translating to shakespearian text.

---
+++ src/shakespearian-translator.test.ts
@@ -0,0 +1,31 @@
+import nock from 'nock'
+import { getShakespearianTranslation } from './shakespearian-translator'
+
+describe('getShakespearianTranslation', () => {
+  it('translates the text to a shakespeare version', async () => {
+    const originalText =
+      'Spits fire that is hot enough to melt boulders.Known to cause forest fires unintentionally'
+
+    const stub = {
+      success: {
+        total: 1,
+      },
+      contents: {
+        translated:
+          'Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally',
+        text:
+          'Spits fire that is hot enough to melt boulders.Known to cause forest fires unintentionally',
+        translation: 'shakespeare',
+      },
+    }
+
+    nock('https://api.funtranslations.com')
+      .get('/translate/shakespeare.json')
+      .query({ text: originalText })
+      .reply(200, stub)
+
+    const translatedText = await getShakespearianTranslation(originalText)
+
+    expect(translatedText).toBe(stub.contents.translated)
+  })
+})
--- src/shakespearian-translator.ts
+++ src/shakespearian-translator.ts
@@ -1,5 +1,33 @@
+import got from 'got'
+import * as z from 'zod'
+
 export type GetShakespearianTranslation = (text: string) => Promise<string>
 
-export const getShakespearianTranslation: GetShakespearianTranslation = (text) => {
-  return Promise.resolve(text)
+const translationServiceUrl = 'https://api.funtranslations.com/translate/shakespeare.json'
+
+export const getShakespearianTranslation: GetShakespearianTranslation = async (text) => {
+  const response = await got.get(translationServiceUrl, { searchParams: { text } }).json()
+
+  const parsedResponse = shakespearianTranslationResponse.safeParse(response)
+
+  if (!parsedResponse.success) {
+    return ''
+  }
+
+  const {
+    data: {
+      contents: { translated: translatedText },
+    },
+  } = parsedResponse
+
+  return translatedText
 }
+
+const shakespearianTranslationResponse = z.object({
+  success: z.object({ total: z.literal(1) }),
+  contents: z.object({
+    translated: z.string(),
+    text: z.string(),
+    translation: z.literal('shakespeare'),
+  }),
+})
