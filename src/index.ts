977590f2d30b290bf0c8801c17975d626dfafbac
Alejandro Gonzalez <agonzalez@findmypast.com>
2020-12-04 22:56:39 +0000

    Plugged getShakespearianDescription to route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,53 @@
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from 'dotenv'
+import express from 'express'
+import { fold } from 'fp-ts/lib/Either'
+import { pipe } from 'fp-ts/lib/function'
+import { getPokemonSpeciesByName } from './apis/pokemon-api/pokemon-species'
+import { getShakespearianTranslation } from './apis/shakespearian-translator'
+import { ApiError, internalError } from './error'
+import { getShakespearianPokemonDescription } from './get-shakespearian-pokemon-description'
 
-dotenv.config();
+dotenv.config()
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT
 
-const app = express();
+const app = express()
 
-app.get("/", (_req, res) => {
-  res.send("Hello world");
-});
+app.get('/pokemon/:pokemonName', async (req, res) => {
+  try {
+    const dependencies = {
+      getShakespearianTranslation: getShakespearianTranslation,
+      getPokemonSpeciesByName: getPokemonSpeciesByName,
+    }
+
+    const getShakespearianDescriptionWithDependencies = getShakespearianPokemonDescription(
+      dependencies
+    )
+
+    const {
+      params: { pokemonName },
+    } = req
+
+    const description = await getShakespearianDescriptionWithDependencies(pokemonName)
+
+    const errorHandler = (error: ApiError) => {
+      switch (error.type) {
+        case 'NotFoundError':
+          return res.status(404).send(error)
+        default:
+          res.status(500).send(error)
+      }
+    }
+
+    pipe(
+      description,
+      fold(errorHandler, (description) => res.send(description))
+    )
+  } catch (err) {
+    res.status(500).send(internalError('Something unexpected happened, please try again later'))
+  }
+})
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
+  console.log(`server started at http://localhost:${port}`)
+})
